7711f671fc86fbdb27f43b7f0321b02fccf1e336
Kural03 <boss8886@gmail.com>
2021-08-13 15:28:22 +0530

    job done

---
+++ src/org/JavaProject/New.java
@@ -0,0 +1,26 @@
+package org.JavaProject;
+
+public class New {
+String language = "";
+	
+	//method1
+	public void tamillanguage() {
+		language = "Tamil";
+		System.out.println("people in southindia speak" + language);
+	}
+	
+	//method2sh
+		public void englishlanguage() {
+			language = "English";
+			System.out.println("people in southindia and northindia speaks"+language);
+			
+			
+}
+		//method3
+		public void hindilanguage() {
+			language = "Hindi";
+			System.out.println("people in northindia speaks" + language);
+		}
+
+
+}
